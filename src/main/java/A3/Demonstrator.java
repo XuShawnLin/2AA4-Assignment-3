3bf06fe59aaf9cd3620859c991a037868123cc27
BrinaLeung <leungw29@mcmaster.ca>
2026-03-19 17:11:16 -0400

    Update Demonstrator.java

--- src/main/java/A3/Demonstrator.java
+++ src/main/java/A3/Demonstrator.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
+import java.util.concurrent.ThreadLocalRandom;
+
+// Tie-breaking (random)
+Rule chosen = bestRules.get(ThreadLocalRandom.current().nextInt(bestRules.size()));
 
 public class Demonstrator {
 
